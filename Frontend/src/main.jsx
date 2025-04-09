a88c495e87402c49a1537bdae89a127088294b9f
Aman20-debug <amanpoddar08072003@gmail.com>
2025-04-09 21:55:49 +0530

    Added update-password route and configure Redux store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
+
+//Am
+import {Provider} from "react-redux"
+import {configureStore} from "@reduxjs/toolkit"
+import rootReducer from "./reducers/index.jsx"
+import {Toaster} from "react-hot-toast"
+import "./index.css"
+
 import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, createRoutesFromElements, RouterProvider,Route} from "react-router-dom"
@@ -10,6 +18,7 @@ import {Login} from "./pages/Login.jsx"
 import { Signup } from './pages/Signup.jsx'
 import { ResetPassword } from './pages/ResetPassword.jsx'
 import { VerifyEmail } from './pages/VerifyEmail.jsx'
+import { UpdatePassword } from './pages/UpdatePassword.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
@@ -18,14 +27,22 @@ const router = createBrowserRouter(
         <Route path='signup' element={<Signup/>}/>
         <Route path='reset-password' element={<ResetPassword/>}/>
         <Route path='verify-email' element={<VerifyEmail/>} />
+        <Route path='update-password/:id' element={<UpdatePassword/>}/>
       </Route>
   )
 )
 
 
+//Am
+const store = configureStore({
+  reducer: rootReducer,
+})
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <RouterProvider router={router}/>
+      <Provider store={store}>
+        <Toaster/>
+        <RouterProvider router={router}/>
+      </Provider>
     </StrictMode>
 )
