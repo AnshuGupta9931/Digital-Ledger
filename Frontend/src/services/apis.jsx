1440bcf012a80315e2674b4f1bce0164ad451177
Aman20-debug <amanpoddar08072003@gmail.com>
2025-06-08 15:45:55 +0530

    Updated apis.jsx

--- Frontend/src/services/apis.jsx
+++ Frontend/src/services/apis.jsx
@@ -8,3 +8,34 @@ export const endpoints = {
     RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
     RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
 }
+
+//TRANSACTION ENDPOINTS
+export const transactionEndpoints = {
+  GET_ALL_TRANSACTIONS_API: BASE_URL + "/transaction/showAllTransactions",
+  CREATE_TRANSACTION_API: BASE_URL + "/transaction/createTransaction",
+  DELETE_TRANSACTION_API: BASE_URL + "/transaction/deleteTransaction",
+  UPDATE_TRANSACTION_API: BASE_URL + "/transaction/updateTransaction",
+  FILTER_TRANSACTIONS_API: BASE_URL + "/transaction/filterTransaction",
+  GET_MONTHLY_SUMMARY_API: BASE_URL + "/transaction/getMonthSummary",
+  GET_PAGINATED_TRANSACTIONS_API: BASE_URL + "/transaction/getPage",
+}
+
+
+//CATEGORY ENDPOINTS
+export const categoryEndpoints = {
+  GET_ALL_CATEGORY_API: BASE_URL + "/category/getAllCategory",
+  CREATE_CATEGORY_API: BASE_URL + "/category/createCategory",
+  // DELETE_TRANSACTION_API: BASE_URL + "/transaction/deleteTransaction",
+  // UPDATE_TRANSACTION_API: BASE_URL + "/transaction/updateTransaction",
+  // FILTER_TRANSACTIONS_API: BASE_URL + "/transaction/filterTransaction",
+  // GET_MONTHLY_SUMMARY_API: BASE_URL + "/transaction/getMonthSummary",
+  // GET_PAGINATED_TRANSACTIONS_API: BASE_URL + "/transaction/getPage",
+}
+
+export const friendEndpoints = {
+    SEND_REQUEST_API: BASE_URL + "/friends/sendFriendRequest",
+    GET_ALL_FRIENDS_API: BASE_URL + "/friends/showAllFriends",
+    GET_PENDING_REQUESTS_API: BASE_URL + "/friends/showAllPendingFriends",
+    ACCEPT_REQUEST_API: BASE_URL + "/friends/acceptFriend",
+    DECLINE_REQUEST_API: BASE_URL + "/friends/declineFriend",
+}
