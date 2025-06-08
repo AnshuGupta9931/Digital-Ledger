e3909b430563acb9705dcae4631099f3e4674384
Aman20-debug <amanpoddar08072003@gmail.com>
2025-06-08 15:35:34 +0530

    Implemented Combine Reducers

--- Frontend/src/reducers/index.jsx
+++ Frontend/src/reducers/index.jsx
@@ -1,11 +1,17 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice.jsx"
 import profileReducer from "../slices/profileSlice.jsx"
+import transactionReducer from "../slices/transactionSlice.jsx"
+import categoryReducer from "../slices/categorySlice.jsx"
+import friendReducer from "../slices/friendSlice.jsx"
 
 
 const rootReducer = combineReducers({
     auth: authReducer,
     profile: profileReducer,
+    transaction: transactionReducer,
+    category: categoryReducer,
+    friend: friendReducer,
 });
 
 export default rootReducer
