8b12693daab11da18dfb96dfe8ce817d5e797164
Aman20-debug <amanpoddar08072003@gmail.com>
2025-06-13 22:35:45 +0530

    added routes for frontend-backend linking

--- Frontend/src/services/apis.jsx
+++ Frontend/src/services/apis.jsx
@@ -20,7 +20,6 @@ export const transactionEndpoints = {
   GET_PAGINATED_TRANSACTIONS_API: BASE_URL + "/transaction/getPage",
 }
 
-
 //CATEGORY ENDPOINTS
 export const categoryEndpoints = {
   GET_ALL_CATEGORY_API: BASE_URL + "/category/getAllCategory",
@@ -38,4 +37,28 @@ export const friendEndpoints = {
     GET_PENDING_REQUESTS_API: BASE_URL + "/friends/showAllPendingFriends",
     ACCEPT_REQUEST_API: BASE_URL + "/friends/acceptFriend",
     DECLINE_REQUEST_API: BASE_URL + "/friends/declineFriend",
+}
+
+export const debtEndpoints = {
+    CREATE_DEBT_API: BASE_URL + "/debts/createDebt",
+    SETTLE_DEBT_API: BASE_URL + "/debts/settleDebt",
+    GET_DEBTS_API: BASE_URL + "/debts/showAllDebt",
+    DELETE_DEBT_API: BASE_URL + "/debts/deleteDebt",
+}
+
+export const savingEndpoints = {
+  CREATE_SAVING_GOAL_API: BASE_URL + "/savings/createSavings",
+  GET_ALL_SAVINGS_API: BASE_URL + "/savings/showAllSavings",
+  UPDATE_SAVING_GOAL_API: BASE_URL + "/savings/updateSavings",
+  DELETE_SAVING_API: BASE_URL + "/savings/deleteSavings",
+  ADD_TO_SAVING_API: BASE_URL + "/savings/addSavings",
+  SUB_FROM_SAVING_API: BASE_URL + "/savings/subSavings",
+}
+
+export const groupEndpoints = {
+  CREATE_GROUP_API: BASE_URL + "/group/createGroup",
+  GET_USER_GROUPS_API: BASE_URL + "/group/showAllGroups",
+  GET_GROUP_BY_ID_API: BASE_URL + "/group/showGroupsById",
+  UPDATE_GROUP_API: BASE_URL + "/group/updateGroup",
+  DELETE_GROUP_API: BASE_URL + "/group/deleteGroup",
 }
